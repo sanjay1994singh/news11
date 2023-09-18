cac8a107373fc03fe94f1ee2c76e2d231c916f7c
sanjay <sanjaypython22@gmail.com>
2023-09-19 00:56:45 +0530

    'news11'

--- homepage/views.py
+++ homepage/views.py
@@ -6,7 +6,13 @@ from .models import Follow
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home_page.html')
+    follow = Follow.objects.first()
+    follow = follow.follow
+
+    context = {
+        'follow': follow
+    }
+    return render(request, 'home_page.html', context)
 
 
 def follow(request):
