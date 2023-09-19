3e9a859917b7953c7a48a27b4b6fab21a44ff755
sanjay <sanjaypython22@gmail.com>
2023-09-19 21:51:40 +0530

    'l'

--- news11/wsgi.py
+++ news11/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for news11 project.
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news11.settings')
+#
+# application = get_wsgi_application()
 
-It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
 
 import os
-
+from static_ranges import Ranges
+from dj_static import Cling, MediaCling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news11.settings')
 
-application = get_wsgi_application()
+
+application = Ranges(Cling(MediaCling(get_wsgi_application())))
