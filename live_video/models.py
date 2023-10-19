6c10d796831dfc1cd81029956b4dc3647e466cc3
sanjay <sanjaypython22@gmail.com>
2023-10-19 21:53:18 +0530

    0

--- live_video/models.py
+++ live_video/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class LiveVideo(models.Model):
     title = models.CharField(max_length=500, null=True, blank=True)
     video_link = models.TextField()
+    video_file = models.FileField(upload_to='live_video', null=True, blank=True)
     views = models.IntegerField()
     likes = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
