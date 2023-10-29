c8013931bd1d4aebaacef1097f552ac19367025f
sanjay <sanjaypython22@gmail.com>
2023-10-29 18:19:16 +0530

    oiuytre

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
-    path('homepage/', views.main_homepage, name='main_homepage'),
+    path('homepage/', views.home_page, name='home_page'),
+    path('', views.main_homepage, name='main_homepage'),
     path('details/<int:id>/', views.details, name='details'),
     path('follow/', views.follow, name='follow'),
     path('watching-views/', views.watching_views, name='watching_views'),
